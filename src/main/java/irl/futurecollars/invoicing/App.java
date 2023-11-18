99641b14fa03ea3b0992a432ea054ef9a8047edd
Jean Farinea <jeanfarinea@gmail.com>
2023-11-18 22:05:25 +0000

    Few changes according to instructor feedback.

--- src/main/java/irl/futurecollars/invoicing/App.java
+++ src/main/java/irl/futurecollars/invoicing/App.java
@@ -7,13 +7,13 @@ package irl.futurecollars.invoicing;
 import irl.futurecollars.invoicing.db.Database;
 import irl.futurecollars.invoicing.db.memory.InMemoryDatabase;
 import irl.futurecollars.invoicing.model.Company;
-// import irl.futurecollars.invoicing.model.Invoice;
-// import irl.futurecollars.invoicing.model.InvoiceEntry;
-// import irl.futurecollars.invoicing.model.Vat;
+import irl.futurecollars.invoicing.model.Invoice;
+import irl.futurecollars.invoicing.model.InvoiceEntry;
+import irl.futurecollars.invoicing.model.Vat;
 import irl.futurecollars.invoicing.service.InvoiceService;
-// import java.math.BigDecimal;
-// import java.time.LocalDate;
-// import java.util.List;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
 public class App {
 
@@ -24,17 +24,17 @@ public class App {
     Company buyer = new Company("5213861303", "ul. Bukowińska 24d/7 02-703 Warszawa, Polska", "iCode Trust Sp. z o.o");
     Company seller = new Company("552-168-66-00", "32-005 Niepolomice, Nagietkowa 19", "Piotr Kolacz Development");
 
-    // List<InvoiceEntry> products = List.of(new InvoiceEntry("Programming course", BigDecimal.valueOf(10000), BigDecimal.valueOf(2300), Vat.VAT_23));
+    List<InvoiceEntry> products = List.of(new InvoiceEntry("Programming course", BigDecimal.valueOf(10000), BigDecimal.valueOf(2300), Vat.VAT_23));
 
-    // Invoice invoice = new Invoice(LocalDate.now(), buyer, seller, products);
+    Invoice invoice = new Invoice(LocalDate.now(), buyer, seller, products);
 
-    // int id = service.save(invoice);
+    int id = service.save(invoice);
 
-    // service.getById(id).ifPresent(System.out::println);
+    service.getById(id).ifPresent(System.out::println);
 
-    // System.out.println(service.getAll());
+    System.out.println(service.getAll());
 
-    // service.delete(id);
+    service.delete(id);
   }
 
 }
